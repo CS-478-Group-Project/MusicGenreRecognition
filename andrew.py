02c6397ff9692919daf2473fef95c14813c03992
Andrew <merrillman32@gmail.com>
2018-11-12 13:48:38 -0700

    added basic setup of andrew class

---
+++ andrew.py
@@ -0,0 +1,15 @@
+class Andrew:
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return "Tempo, MFCC1"
+
+    def execute(fullSong, songSection, sr):
+        
+
+
+me = Andrew()
+string = str(me)
+print(string)
