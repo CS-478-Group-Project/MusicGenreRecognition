356778410f311164c729985d3a10618e64d2497f
Gibson Ainge <gainge@gmail.com>
2018-11-13 12:02:37 -0700

    Added basic str and method stubs

--- Gibson.py
+++ Gibson.py
@@ -1,12 +1,42 @@
-
+import numpy as np
+from pyAudioAnalysis import audioFeatureExtraction, audioBasicIO
+from scipy import signal
+from scipy.signal import butter, lfilter
+from scipy.signal import freqz
+from scipy.fftpack import fft,fftfreq
+import librosa
 
 
 class Gibson:
     def __init__(self):
         return
 
-    def extract(self, sample, song, rate):
+    def __str__(self):
+        return "Zero Crossing Rate, Short Time Energy, Spectral Roll Off, Entropy, Spectral Flux, " \
+                "b1, b2, b3, b4, b5, b6, b7"
+
+    def extract(self, section, full_song, sampling_rate):
+        self.section = section
+        self.full_song = full_song
+        self.sampling_rate = sampling_rate
+
         return [1,2,3]
 
-    def __str__(self):
-        return "val1, val2, val3"
+
+    def zcr():
+        return
+
+    def short_time_energy():
+        return
+
+    def spectral_roll_off():
+        return
+
+    def entropy():
+        return
+
+    def spectral_flux():
+        return
+
+    def frequency_bins():
+        return
