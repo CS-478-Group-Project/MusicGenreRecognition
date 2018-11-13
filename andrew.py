015541e74b6d8e87d00f5a1c528eb0e2f6374ff9
Andrew <merrillman32@gmail.com>
2018-11-12 17:39:41 -0700

    added chroma features. Still need to see if these are the best features to extract from MFCC and Chroma data

--- andrew.py
+++ andrew.py
@@ -7,7 +7,9 @@ class Andrew:
         pass
 
     def __str__(self):
-        return "Tempo, Total Beats, Average Beats, MFCC1"
+        return "Tempo, Total beats, Average beats, MFCC mean, MFCC std, MFCC variance, " \
+                "MFCC delta mean, MFCC delta std, MFCC delta variance, " \
+                "Chroma stft mean, Chroma stft std, chroma stft variance"
 
     """
     :param full: full current song
@@ -23,6 +25,10 @@ class Andrew:
         self.full = full
         self.section = section
         self.sr = sr
+        features = []
+        features += self.tempo()
+        features += self.MFCC()
+        features += self.Chroma()
 
     """
     :return: The BPM, total beats and average beats
@@ -38,6 +44,24 @@ class Andrew:
     """
     :return: The MFCC features
     :rtype: list of floats
+
+    An MFC is a representation of the shortterm power spectrum of a sound based on a 
+    log power spectrums linear cosine transform on a nonlinear mel scale of frequency...
     """
     def MFCC(self):
-        pass
+        mfcc = librosa.feature.mfcc(y=self.section, sr=self.sr)
+        mfcc_delta = librosa.feature.delta(mfcc)
+        mfcc_mean = np.mean(mfcc)
+        mfcc_std = np.std(mfcc)
+        mfcc_var = np.var(mfcc)
+        mfcc_delta_mean = np.mean(mfcc_delta)
+        mfcc_delta_std = np.std(mfcc_delta)
+        mfcc_delta_var = np.var(mfcc_delta)
+        return [mfcc_mean, mfcc_std, mfcc_var, mfcc_delta_mean, mfcc_delta_std, mfcc_delta_var]
+
+    def Chroma(self):
+        chroma_stft = librosa.feature.chroma_stft(y=self.section, sr=self.sr)
+        chroma_stft_mean = np.mean(chroma_stft)
+        chroma_stft_std = np.std(chroma_stft)
+        chroma_stft_var = np.var(chroma_stft)
+        return [chroma_stft_mean, chroma_stft_std, chroma_stft_var]
