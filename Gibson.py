51f3072a17ecfe0d3bc4cd5e7c04a49a6d8e106a
Gibson Ainge <gainge@gmail.com>
2018-11-13 11:55:06 -0700

    Added basic template for extraction module

---
+++ Gibson.py
@@ -0,0 +1,12 @@
+
+
+
+class Gibson:
+    def __init__(self):
+        return
+
+    def extract(self, sample, song, rate):
+        return [1,2,3]
+
+    def __str__(self):
+        return "val1, val2, val3"
